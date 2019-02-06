6ecc80db9aa92ca39e2c969a27dcb3e53c7b974a
Alexandr <nvil05@itgrade.ru>
2019-02-06 21:23:21 +0000

    lesson5.1

--- index.php
+++ index.php
@@ -1,58 +1,22 @@
-<?php 
-require_once $_SERVER['DOCUMENT_ROOT'] . ('/app/template/header.php');
-?>
-				<?php if ($_GET['login'] == 'yes') :?>
-	                <td class="right-collum-index">
-				
-						<div class="project-folders-menu">
-							<ul class="project-folders-v">
-							<li class="project-folders-v-active"><span>Авторизация</span></li>
-							<li><a href="#">Регистрация</a></li>
-							<li><a href="#">Забыли пароль?</a></li>
-							</ul>
-						<div style="clear: both;"></div>
-						</div>
-						<div class="index-auth">
-							<table width="100%" border="0" cellspacing="0" cellpadding="0">
-
-								<?php if (!empty($_POST['login'])) {
-
-									$arLogin = include $_SERVER['DOCUMENT_ROOT'] . ('/include/login.php');
-									$arPassw = include $_SERVER['DOCUMENT_ROOT'] . ('/include/password.php');
-
-									include $_SERVER['DOCUMENT_ROOT'] . ('/include/controller.php');
-
-									$result = check($arLogin, $arPassw, $_POST);
-
-									if ($result) {
-
-										include $_SERVER['DOCUMENT_ROOT'] . ('/include/success.php');
-									
-									} else {
-
-										echo "Неверный логин или пароль";
-									}
-								}?>
-									<form method="POST" action="index.php?login=yes" >
-
-										<tr>
-											<td class="iat">Ваш e-mail: <br /> <input id="login_id" size="30" value="<?= $_POST['login'] ? htmlspecialchars($_POST['login']) : ' '; ?>" name="login" /></td>
-										</tr>
-										<tr>
-											<td class="iat">Ваш пароль: <br /> <input id="login_id" size="30" value="<?=  $_POST['password'] ? htmlspecialchars($_POST['password']) : ''; ?>" name="password" /></td>
-										</tr>
-										<tr>
-											<td><input type="submit" value="Войти" /></td>
-										</tr>
-									</form>
-									
-							</table>
-						</div>
-					
-					</td>
-				<?php endif ?>
-            </tr>
-        </table>
-<?php
-require_once $_SERVER['DOCUMENT_ROOT'] . ('/app/template/footer.php');
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Lesson 5</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<script type="text/javascript" src="/js/main.js"></script>
+</head>
+<body>
+	<pre>
+		<a href="print_image.php">Страница просмотра загруженных изображений</a>
+	</pre>
+	<div>
+		<p id="message"></p>
+	</div>
+	<form  id="frm_file" enctype="multipart/form-data">
+		<div>
+			<input type="file" name="myfile[]" id="myfile" required multiple="true" accept="image/png, image/jpeg"/>
+			<input type="button" name="upload" value="Загрузить"/>
+		</div>
+	</form>
+</body>
+</html>
